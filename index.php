57b9146609cf1fb4702e5d34cb51cc6f2f6dbc92
gianlucafalaschi <gian100gian@outlook.it>
2024-05-15 17:30:36 +0200

    Products.php

--- index.php
+++ index.php
@@ -1,6 +1,7 @@
 <?php
 
 require_once __DIR__ . '/Models/Emporium.php';
+require_once __DIR__ . '/Models/Products.php';
 
 // creo un new Emporium e fornisco gli attributi obbligatori del construct 
 $emporium = new Emporium('La casa degli animali', 'via degli Alberi 5');
@@ -12,5 +13,9 @@ $emporium->setCategory('Gatti');
 
 var_dump($emporium);
 
+$products = new Products();
+$products->brand = 'Nutrix';
+
+var_dump($products);
 ?>
 
