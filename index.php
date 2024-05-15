fcf77f5733db6fb4a4e4a2e5ef9c02efcb122369
gianlucafalaschi <gian100gian@outlook.it>
2024-05-15 21:26:47 +0200

    cards printed

--- index.php
+++ index.php
@@ -42,5 +42,50 @@ $fleaCollar->setCategory('Gatti');
 
 var_dump($fleaCollar);
 
+
+$products = [
+    $kibble,
+    $kennel,
+    $mouse,
+    $fleaCollar
+]
+
 ?>
 
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <!--  Bootstrap -->
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
+    <!--/  Bootstrap -->
+    <title>Document</title>
+</head>
+<body>
+    <div class="container">
+        <div class="row">
+            <?php foreach($products as $product) { ?><div class="col mb-3">
+                <div class="card" style="width: 18rem;">
+                    <img src="..." class="card-img-top" alt="...">
+                    <div class="card-body">
+                        <h5 class="card-title"><?php echo $product->type ?></h5>
+                    </div>
+                    <ul class="list-group list-group-flush">
+                        <li class="list-group-item"><?php echo $product->price ?> Euro</li>
+                        <li class="list-group-item"><?php echo $product->name ?></li>
+                        <li class="list-group-item"><?php echo $product->address ?></li>
+                        <li class="list-group-item"><?php echo $product->getGenre() ?></li>
+                        <li class="list-group-item">Anno di fondazione: <?php echo $product->foundationYear ?></li>
+                        <li class="list-group-item"><?php echo $product->getCategory() ?></li>
+                        
+                    </ul>
+                </div>
+            </div> <?php } ?>
+        </div>
+    </div>
+
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
+</body>
+</html>
