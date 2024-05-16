a33c7409e58bc9d30c733d8c2686bd6c72e7b3f0
gianlucafalaschi <gian100gian@outlook.it>
2024-05-16 20:01:22 +0200

    trait created and imported

--- Models/Products.php
+++ Models/Products.php
@@ -1,9 +1,12 @@
 <?php
 
 require_once __DIR__ . '/Emporium.php';
-
+require_once __DIR__ . '/../Traits/BrandCustomerService.php';
 
 class Products extends Emporium {
+
+    use BrandCustomerService;
+
     public $type;
     public $price;
 
---
+++ Traits/BrandCustomerService.php
@@ -0,0 +1,30 @@
+<?php
+
+trait BrandCustomerService {
+    protected $email;
+    protected $phone;
+
+ /**   
+ * This function returns the value of the procted attribute $email
+ * 
+ * @return int The instance email
+ */
+
+public function getEmail() {
+    return $this->email;
+}
+
+/**
+ * Set the $email attribute
+ * 
+ * @param int $newEmail The new email to be set in the instance
+   */
+
+public function setEmail($newEmail) {
+    $this->email = $newEmail;
+}
+
+
+}
+
+?>
